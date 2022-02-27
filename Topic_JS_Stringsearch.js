67b429022d1ad9319d4ae6b25b39fffb0495186e
Himanshu <himanshug706@gmail.com>
2022-02-27 13:23:51 +0530

    JS String search

---
+++ Topic_JS_Stringsearch.js
@@ -0,0 +1,20 @@
+//JS String Search
+// Indexof()
+let a = "This is my JS training"
+console.log(a.indexOf('JS'))
+
+//lastindex()
+let b = "This is my JS training"
+console.log(b.indexOf("is"))
+
+//search function
+let c = "This is my JS training"
+console.log(c.search("my"))
+
+//match function
+let v = "this is my training"
+console.log(v.match(/is/gi))
+
+//startswith, endswith function
+let text = "Hello world, welcome to the universe";
+console.log(text.endsWith("universe"))
