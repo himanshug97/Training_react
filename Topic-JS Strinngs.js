38d7e0fb9755dcba12e3a7883d61262ea06ed2b1
Himanshu <himanshug706@gmail.com>
2022-02-27 12:18:29 +0530

    JS string topic

---
+++ Topic-JS Strinngs.js
@@ -0,0 +1,11 @@
+
+//string as objects
+let x = "John";
+let y = new String("John");
+console.log(typeof x + '<br>' + typeof y);
+
+//Equal and not equal operator
+ let a = "John";
+ let b = new String ("John");
+ console.log(a==b)
+ console.log(a===b)
