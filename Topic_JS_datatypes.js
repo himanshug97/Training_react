4e10ff2c8a225a64c034f7a29ba4b68b0b6526de
Himanshu <himanshug706@gmail.com>
2022-03-01 19:23:05 +0530

    JS Data Types

---
+++ Topic_JS_datatypes.js
@@ -0,0 +1,15 @@
+//JS data types
+
+//When adding a number and a string, JavaScript will treat the number as a string
+let q = 16 + "himanshu";
+let w = 16 + 4 + "himanshu"
+console.log(q)
+console.log(w)
+
+//Numbers can be written with, or without decimals:
+let e = 34.00;
+let e1 = 34;
+let e2 = 3.14;
+console.log(e, e1, e2)
+
+//boolean
